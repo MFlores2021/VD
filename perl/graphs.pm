fb9de34f4ed6be90d1722161efe7922eff0db52d
MFlores2021 <mire_flor3@hotmail.com>
2020-07-11 13:15:03 -0700

    spikes linear graphs

--- perl/graphs.pm
+++ perl/graphs.pm
@@ -9,33 +9,56 @@ use GD;
 use GD::Graph::bars;
 use GD::Graph::Data;
 use GD::Graph::lines;
+use GD::Graph::points;
+use List::Util qw(min max);
+use Scalar::Util qw(looks_like_number);
 
 sub create_spiking_sum {
 	my $dir = "C:\\Users\\mire_\\VDW\\results\\testjune";
 	my $spike = "ac,acan,cajac";
+	my $sum_file = catfile($dir ,'Summary.tsv');
 	
 	my @spikes = split /,/, $spike;
 	my $spikes = scalar(@spikes);
 	
-	if (-e -s catfile($dir ,'Summary.tsv') && $spikes > 0){
+	if (-e -s $sum_file && $spikes > 0){
 		
-		# Draw spikes
-		my $data = GD::Graph::Data-> new() or die;
-		$data->read(file => catfile($dir ,'Summary.tsv'), delimiter => '\t');
-		my @spike_col;
+		my (@spike_col,@spike_col_n);
+		my @fastq;
 		my $column=8;
-		for (0 .. $spikes-1){ push @spike_col, $column+($_*2);}
-		$data->wanted(@spike_col);
-		my $result = _draw_lines($data,$dir,'Spike in summary','spike_sum.png',\@spikes);
+		my $column_n=$column-1;
+		for (0 .. $spikes-1){
+			push @spike_col, $column+($_*2);
+			push @spike_col_n, $column_n+($_*2);
+		}
+	
+		open(FILE,$sum_file) || die "WRONG FILE";
+		my (@data,@data_n);
+		my $max = 0;
+		my $max_n = 0;
 		
-		# Draw Norm Spikes
-		my $data_n = GD::Graph::Data-> new() or die;
-		$data_n->read(file => catfile($dir ,'Summary.tsv'), delimiter => '\t');
-		my @spike_col_n;
-		my $column_n=7;
-		for (0 .. $spikes-1){ push @spike_col_n, $column_n+($_*2);}
-		$data_n->wanted(@spike_col_n);
-		my $result_n = _draw_lines($data_n,$dir,'Normalized spike in summary','norm_spike_sum.png',\@spikes);
+		while(my $line = <FILE>){
+			chomp $line;
+			next if $line =~ /^\s*$/;
+			my @line = split(/\t/,$line);
+			my (@value,@value_n);
+			push @fastq, $line[0] if (!($line =~ /^File/));
+			foreach(@spike_col){
+				$line[$_] =~ s/#\ Spikes:\ //g;
+				push @value, $line[$_];
+				if(looks_like_number($line[$_]) && $max < $line[$_]) { $max = $line[$_]; }
+			}
+			foreach(@spike_col_n){
+				$line[$_] =~ s/Norm.\ Spike:\ //g;
+				push @value_n, $line[$_];
+				if(looks_like_number($line[$_]) && $max_n < $line[$_]) { $max_n = $line[$_]; }
+			}
+			push @data, [@value];
+			push @data_n, [@value_n];
+		}
+	
+		my $result = _draw_lines(\@data,$dir, 'Spikes summary','spike_sum.png',\@fastq,$max);
+		my $result_n = _draw_lines(\@data_n,$dir,'Normalized spikes summary','norm_spike_sum.png',\@fastq,$max_n);
 	}
 	return 1;
 }
@@ -60,11 +83,11 @@ sub _draw_lines {
 	my $dir = shift;
 	my $title = shift;
 	my $out_name = shift;
-	my $spikes = shift;
-	my @spikes = @$spikes;
+	my $fastq = shift;
+	my $max = shift;
+	my @fastq = @$fastq;
 
-	my $graph = GD::Graph::lines->new(600,900);
-	my $max = $data->get_min_max_y_all(); print Dumper \$max;
+	my $graph = GD::Graph::lines->new(400,450);
 
 	$graph->set(
 		x_label         => 'Samples',
@@ -85,8 +108,8 @@ sub _draw_lines {
 		r_margin=>10,
 		accentclr    => 'white',
 	) or die $graph->error;
-	
-	$graph->set_legend(@spikes);
+
+	$graph->set_legend(@fastq);
 	$graph->set_legend_font('gdMediumBoldFont', 20);
 	$graph->set_title_font('gdGiantFont');
 	$graph->set_y_label_font('gdMediumBoldFont');
@@ -111,8 +134,8 @@ sub _draw_bars {
 	my $x = shift;
 	my @x_axes = @$x;
 
-	my $graph = GD::Graph::bars->new(800,1200);
-	my $max = $data->get_min_max_y_all()->[1]; print Dumper $max;
+	my $graph = GD::Graph::bars->new(400,450);
+	my $max = $data->get_min_max_y_all(); print Dumper $max;
 
 	$graph->set(
 			x_label         => 'Samples',
@@ -150,8 +173,9 @@ sub _draw_bars {
 	return 1;
 }
 
+
 sub trim { my $s = shift; if($s){ $s =~ s/^\s+|\s+$//g; }; return $s };
 
-create_spiking_sum();
+#create_spiking_sum();
 graph_cumulative_clean_reads();
 1;
