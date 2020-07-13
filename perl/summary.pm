9db04155727d3cf4c79da888f362d1048307d7bd
MFlores2021 <mire_flor3@hotmail.com>
2020-07-12 17:45:03 -0700

    clean code

--- perl/summary.pm
+++ perl/summary.pm
@@ -8,24 +8,38 @@ sub create_html {
 	my $dir = shift;
 	my $spike = shift;
 	my @files = @_;
-	
+
+	my $sum_file = catfile($dir,"Summary.tsv");	
 	my @spikes = split /,/, $spike;
-	print Dumper \@files;
-	print Dumper $spike;
+	# print Dumper \@files;
+	# print Dumper $spike;
 	print scalar @spikes;
 	open my $fh, '>', catfile($dir,"Summary.html") or warn "couldn't open: $!";
 	
 	print $fh '<!DOCTYPE html>
 		<html>
 		<head>
-		<title>VDW</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+			<title>VDW</title>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+			<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+			<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+			<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+			<link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
 		</head>
 		<body>
+		<script>
+			$(document).ready(function() {
+				$("#sumTable").DataTable({
+					scrollX : true,
+					responsive : true;
+				});
+			} );
+		</script>
 		<style type="text/css">
 		@media all {
 			.lightbox { display: none; }
@@ -45,6 +59,7 @@ sub create_html {
 	foreach my $file (@files){
 		_print_name($file,$fh);
 	}
+	_print_table($fh,$sum_file);
 	
 	_print_summaries($dir,$fh);
 	
@@ -62,6 +77,37 @@ sub _print_name {
 	print $fh $file . "\n";
 }
 
+sub _print_table {
+	my $fh = shift;
+	my $sum_file = shift;
+	
+	my $table = '<div class="row"><div class="col-lg-12">';
+
+	$table .= '<table class="table table-striped table-bordered table-hover" width="100%" id="sumTable" style="width:100%">';
+
+	open(FILE,$sum_file) || die "WRONG FILE";
+		my (@data,@data_n);
+				
+		while(my $line = <FILE>){
+			chomp $line;
+			next if $line =~ /^\s*$/;
+			if ($line =~ /^File/){ $table .= '<thead>'; }
+			$table .= '<tr>';
+			my @col = split(/\t/,$line);
+			
+			foreach(@col){
+				if ($line =~ /^File/){ $table .= '<th>' . $_ . '</th>'; }
+				else { $table .= '<td>' . $_ . '</td>' };
+			}
+			
+			if ($line =~ /^File/){ $table .= '</thead>'; }
+			$table .= '</tr>';
+		}
+	$table .= '</table>';
+	$table .= '</div></div>';
+	print $fh $table . "\n";
+
+}
 sub _print_summaries {
 	my $dir = shift;
 	my $fh = shift;
