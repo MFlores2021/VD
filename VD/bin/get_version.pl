78b1332b00b01b70c5215d18123e86e08f4a2e45
Mirella Flores <mire_flor3@hotmail.com>
2021-11-04 19:02:53 -0400

    fixes get version with http

--- VD/bin/get_version.pl
+++ VD/bin/get_version.pl
@@ -3,32 +3,22 @@
 use strict;
 use warnings;
 use Net::FTP;
+use HTTP::Request;
+use LWP::UserAgent;
+require HTTP::Request;
+use Data::Dumper;
+use List::Util qw(max);
 
-my $ftp_site     = 'bioinfo.bti.cornell.edu';
-my $ftp_dir     = 'pub/program/VirusDetect/virus_database';
-my $glob         = 'v*';
+my $ftp_site     = 'http://bioinfo.bti.cornell.edu/';
+my $ftp_dir     = 'ftp/program/VirusDetect/virus_database';
 
-my $ftp = Net::FTP->new($ftp_site) 
+my $request = HTTP::Request->new(GET => $ftp_site . $ftp_dir)
     or die "Could not connect to $ftp_site: $!";
+my $ua = LWP::UserAgent->new;
+my $response = $ua->request($request);
+my $sth = $response->content;
 
-$ftp->login() 
-    or die "Could not login to $ftp_site with user  $!";
+my @versions = $sth =~ m|<a href="v(.+?)/">v(.+?)/</a>|g;
+my $folder = max(@versions); 
 
-$ftp->cwd($ftp_dir) 
-    or die "Could not change remote working " . 
-             "directory to $ftp_dir on $ftp_site";
-
-my @remote_files = $ftp->ls($glob);
-my @sorted_files = reverse sort @remote_files;
-my $folder = $sorted_files[0];
-
-print $folder;
-
-$ftp->quit();
-
-# print "v229";
-
-    
-
-
-
+print "v" . $folder;
